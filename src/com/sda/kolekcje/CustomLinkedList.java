2dd42337be1e4013a0bbedd555a8e81c9282303b
tomlach <tlachawczak@gmail.com>
2018-10-21 13:38:21 +0200

    aktualizacje po utworzeniu klas

---
+++ src/com/sda/kolekcje/CustomLinkedList.java
@@ -0,0 +1,23 @@
+package com.sda.kolekcje;
+
+public class CustomLinkedList<E> implements CustomList {
+
+    public CustomLinkedList() {
+        super();
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -1,8 +1,21 @@
 package com.sda.kolekcje;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+// Arraylist - implementacja tablicową
+// linkedlist - implementacja wiązaną
+
+        List<String> list = new LinkedList<>();
+        list.add("Jan");
+        list.add("Marcin");
+        list.add("Karol");
+
+        for(String l: list){
+            System.out.println(l);
+        }
     }
 }
